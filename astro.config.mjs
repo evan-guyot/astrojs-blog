32dba60aa0f2bed61bf9fbb4330ca28d52038e2d
Evan Guyot <evanguyot.pro@gmail.com>
2026-01-20 10:42:38 +0100

    feat: ajout de tailwind et redesign

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import tailwindcss from "@tailwindcss/vite";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astrojs-blog-eguyot.netlify.app",
   integrations: [preact()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -7,9 +7,15 @@ export default function Greeting({ messages }) {
   const [greeting, setGreeting] = useState(messages[0]);
 
   return (
-    <div>
-      <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>New Greeting</button>
+    <div class="relative w-fit m-auto rounded-full px-3 py-1 text-sm/6 animate-bounce cursor-pointer text-gray-400 dark:text-gray-600 ring-1 ring-black/10 hover:ring-black/20 dark:ring-white/10 dark:hover:ring-white/20">
+      {greeting} and Thank you for visiting.{" "}
+      <span
+        onClick={() => setGreeting(randomMessage())}
+        class="font-semibold text-indigo-400"
+      >
+        <span aria-hidden="true" class="absolute inset-0"></span>Click for a new
+        greeting !
+      </span>
     </div>
   );
 }
